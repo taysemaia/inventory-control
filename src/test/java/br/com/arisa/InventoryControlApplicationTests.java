76b6ff9a0d179ac2894f4b75e54ed6d2c2e20ed9
Tayse <53482565+taysemaia@users.noreply.github.com>
2024-01-29 18:46:16 -0300

    Add files via upload

---
+++ src/test/java/br/com/arisa/InventoryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
